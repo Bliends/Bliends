815504ce34485d5782583e6ce16e80d290c5c448
godsejeong <dbdnpxm@naver.com>
2018-09-01 20:54:18 +0900

    add bottomnavigetionview

--- app/src/main/java/com/bliends/pc/bliends/activity/MainActivity.kt
+++ app/src/main/java/com/bliends/pc/bliends/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bliends.pc.bliends.activity
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.bliends.pc.bliends.R
@@ -9,6 +10,13 @@ import com.bliends.pc.bliends.data.User
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
+import android.support.design.internal.BottomNavigationItemView
+import java.lang.reflect.AccessibleObject.setAccessible
+import java.lang.reflect.Array.setBoolean
+import android.support.design.internal.BottomNavigationMenuView
+import android.support.design.widget.BottomNavigationView
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,5 +30,31 @@ class MainActivity : AppCompatActivity() {
             toast("로그아웃")
             finish()
         }
+        disableShiftMode(mainBottomNav)
     }
+
+    @SuppressLint("RestrictedApi")
+    fun disableShiftMode(view: BottomNavigationView) {
+        val menuView = view.getChildAt(0) as BottomNavigationMenuView
+        try {
+            val shiftingMode = menuView.javaClass.getDeclaredField("mShiftingMode")
+            shiftingMode.isAccessible = true
+            shiftingMode.setBoolean(menuView, false)
+            shiftingMode.isAccessible = false
+            for (i in 0 until menuView.childCount) {
+                val item = menuView.getChildAt(i) as BottomNavigationItemView
+
+                item.setShiftingMode(false)
+                // set once again checked value, so view will be updated
+
+                item.setChecked(item.itemData.isChecked)
+            }
+        } catch (e: NoSuchFieldException) {
+            Log.e("BNVHelper", "Unable to get shift mode field", e)
+        } catch (e: IllegalAccessException) {
+            Log.e("BNVHelper", "Unable to change value of shift mode", e)
+        }
+
+    }
+
 }
