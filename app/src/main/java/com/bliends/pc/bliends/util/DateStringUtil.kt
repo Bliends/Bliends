ecf3180909e1b00f20d6aea0f4d8e534af171e2e
Sungyong Son <space010127@gmail.com>
2018-09-25 19:09:43 +0900

    [Fix] DateStringUtil

--- app/src/main/java/com/bliends/pc/bliends/util/DateStringUtil.kt
+++ app/src/main/java/com/bliends/pc/bliends/util/DateStringUtil.kt
@@ -9,7 +9,7 @@ object DateStringUtil{
         replaceBuffer.append(" ")
         replaceBuffer.append(date.substring(5, 7) + "월")
         replaceBuffer.append(" ")
-        replaceBuffer.append(date.substring(8, 9) + "일")
+        replaceBuffer.append(date.substring(8, 10) + "일")
         replaceBuffer.append(" ")
         replaceBuffer.append(date.substring(11, 13) + "시")
         replaceBuffer.append(" ")
